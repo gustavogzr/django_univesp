4cd005532d32e09756144d46e88c248c6343680e
gustavogzr <gustavogzr@hotmail.com>
2022-09-18 23:04:57 -0300

    Upload 2

---
+++ helloworld/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class AlunoUnivesp(models.Model):
+    nome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    sobrenome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    matricula = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+    objetos = models.Manager()
